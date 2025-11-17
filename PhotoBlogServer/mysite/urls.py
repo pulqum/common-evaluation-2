44a36a79d5ae2fd1a88c10fc23fe48726790a656
pulqum <150813710+pulqum@users.noreply.github.com>
2025-11-17 23:45:54 +0900

    Update .gitignore and add JWT auth endpoint
    Expanded .gitignore to exclude YOLOv5-related files and directories. Added JWT token authentication endpoint to Django URLs using rest_framework_simplejwt.

--- PhotoBlogServer/mysite/urls.py
+++ PhotoBlogServer/mysite/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api-token-auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', include('blog.urls')),
 ]
 
